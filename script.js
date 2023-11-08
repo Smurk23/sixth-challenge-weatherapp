b5dc02a5d64a250aa53ff6e1d9a20692e28d3721
Smurk23 <xingcantres@gmail.com>
2023-11-07 22:33:40 -0500

    finished off adding forecast

--- script.js
+++ script.js
@@ -63,14 +63,146 @@ imageEl.setAttribute('alt', description);
 cityDateIconEl.append(imageEl);
 }
 
-function displayFiveDayWeather(city, data) {
+function displayFiveDayWeather1 (city, data) {
+let temp = data[8].main.temp;
+let windspeed = data[8].wind.speed;
+let humidity = data[8].main.humidity;
+let date = data[8].dt_txt.split(' ')[0];
+let weatherIconUrl = `https://openweathermap.org/img/w/${data[10].weather[0].icon}.png`;
+let description = data[8].weather[0].description;
 
+let tempEl = document.getElementById("temp1");
+tempEl.textContent = `Temp:  ${temp}  F`;
+
+let windEl = document.getElementById("wind1");
+windEl.textContent = `Wind Speed:  ${windspeed}  MPH`;
+
+let humidityEl = document.getElementById('humidity1');
+humidityEl.textContent = `Humidity: ${humidity} %`;
+
+let dateEl = document.getElementById('date1');
+dateEl.textContent = date;
+
+let imageEl = document.getElementById('icon1')
+imageEl.setAttribute('src', weatherIconUrl);
+imageEl.setAttribute('alt', description);
+// cityDateIconEl.append(imageEl);
 }
+
+function displayFiveDayWeather2 (city, data) {
+    let temp = data[16].main.temp;
+    let windspeed = data[16].wind.speed;
+    let humidity = data[16].main.humidity;
+    let date = data[16].dt_txt.split(' ')[0];
+    let weatherIconUrl = `https://openweathermap.org/img/w/${data[16].weather[0].icon}.png`;
+    let description = data[16].weather[0].description;
+    
+    let tempEl = document.getElementById("temp2");
+    tempEl.textContent = `Temp:  ${temp}  F`;
+    
+    let windEl = document.getElementById("wind2");
+    windEl.textContent = `Wind Speed:  ${windspeed}  MPH`;
+    
+    let humidityEl = document.getElementById('humidity2');
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+    
+    let dateEl = document.getElementById('date2');
+    dateEl.textContent = date;
+    
+    let imageEl = document.getElementById('icon2')
+    imageEl.setAttribute('src', weatherIconUrl);
+    imageEl.setAttribute('alt', description);
+    // cityDateIconEl.append(imageEl);
+    }
+    
+    function displayFiveDayWeather3 (city, data) {
+        let temp = data[24].main.temp;
+        let windspeed = data[24].wind.speed;
+        let humidity = data[24].main.humidity;
+        let date = data[24].dt_txt.split(' ')[0];
+        let weatherIconUrl = `https://openweathermap.org/img/w/${data[24].weather[0].icon}.png`;
+        let description = data[24].weather[0].description;
+        
+        let tempEl = document.getElementById("temp3");
+        tempEl.textContent = `Temp:  ${temp}  F`;
+        
+        let windEl = document.getElementById("wind3");
+        windEl.textContent = `Wind Speed:  ${windspeed}  MPH`;
+        
+        let humidityEl = document.getElementById('humidity3');
+        humidityEl.textContent = `Humidity: ${humidity} %`;
+        
+        let dateEl = document.getElementById('date3');
+        dateEl.textContent = date;
+        
+        let imageEl = document.getElementById('icon3')
+        imageEl.setAttribute('src', weatherIconUrl);
+        imageEl.setAttribute('alt', description);
+        // cityDateIconEl.append(imageEl);
+        }
+    
+        function displayFiveDayWeather4 (city, data) {
+            let temp = data[32].main.temp;
+            let windspeed = data[32].wind.speed;
+            let humidity = data[32].main.humidity;
+            let date = data[32].dt_txt.split(' ')[0];
+            let weatherIconUrl = `https://openweathermap.org/img/w/${data[32].weather[0].icon}.png`;
+            let description = data[32].weather[0].description;
+            
+            let tempEl = document.getElementById("temp4");
+            tempEl.textContent = `Temp:  ${temp}  F`;
+            
+            let windEl = document.getElementById("wind4");
+            windEl.textContent = `Wind Speed:  ${windspeed}  MPH`;
+            
+            let humidityEl = document.getElementById('humidity4');
+            humidityEl.textContent = `Humidity: ${humidity} %`;
+            
+            let dateEl = document.getElementById('date4');
+            dateEl.textContent = date;
+            
+            let imageEl = document.getElementById('icon4')
+            imageEl.setAttribute('src', weatherIconUrl);
+            imageEl.setAttribute('alt', description);
+            // cityDateIconEl.append(imageEl);
+            }
+
+            function displayFiveDayWeather5 (city, data) {
+                let temp = data[39].main.temp;
+                let windspeed = data[39].wind.speed;
+                let humidity = data[39].main.humidity;
+                let date = data[39].dt_txt.split(' ')[0];
+                let weatherIconUrl = `https://openweathermap.org/img/w/${data[39].weather[0].icon}.png`;
+                let description = data[39].weather[0].description;
+                
+                let tempEl = document.getElementById("temp5");
+                tempEl.textContent = `Temp:  ${temp}  F`;
+                
+                let windEl = document.getElementById("wind5");
+                windEl.textContent = `Wind Speed:  ${windspeed}  MPH`;
+                
+                let humidityEl = document.getElementById('humidity5');
+                humidityEl.textContent = `Humidity: ${humidity} %`;
+                
+                let dateEl = document.getElementById('date5');
+                dateEl.textContent = date;
+                
+                let imageEl = document.getElementById('icon5')
+                imageEl.setAttribute('src', weatherIconUrl);
+                imageEl.setAttribute('alt', description);
+                // cityDateIconEl.append(imageEl);
+                }
+        
+    
 
 function displayItems(city, data) {
     //to be done
     displayTodayWeather(city, data.list[0]);
-    displayFiveDayWeather(city, data.list);
+    displayFiveDayWeather1(city, data.list);
+    displayFiveDayWeather2(city, data.list);
+    displayFiveDayWeather3(city, data.list);
+    displayFiveDayWeather4(city, data.list);
+    displayFiveDayWeather5(city, data.list);
 
 }
 
@@ -92,7 +224,7 @@ function getWeather(data) {
             displayItems(city, data);
         })
         .catch(function (err) {
-            console.error(err);
+            console.log(err);
         });
 }
 
